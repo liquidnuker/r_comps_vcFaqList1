51bb67c8e7296ce71a46f51013f3a2db485c8002
administrator <liquidnuker@users.noreply.github.com>
2017-07-17 08:55:49 -0700

    initial commit

---
+++ src/vue-components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- vcFaqList1-01 placeholder -->
+    <h2 class="item-id">vcFaqList1-01</h2>
+    <div class="row">
+      <div class="col-sm-3">39</div>
+      <div class="col-sm-9">
+      <!-- vcFaqlist1-01 mount -->
+        <div id="vcFaqList1-01_mount"></div>
+      <!-- end vcFaqlist1-01 mount -->
+      </div>
+    </div>
+    <!-- end vcFaqList1-01 placeholder -->
+  </div>
+</template>
+<script>
+import {inject} from "../js/componentinjector.js";
+  export default {
+    data () {
+      return {    
+      }
+    },
+    mounted: function () {
+      this.mountFaqList1_01();
+    },
+    methods: {
+      mountFaqList1_01: function() {
+        const vcFaqlist1_01 = resolve => {
+          require.ensure(['./vcFaqlist1_01.vue'], () => {
+            resolve(require('./vcFaqlist1_01.vue'))
+          })
+        };
+        inject("#vcFaqList1-01_mount", vcFaqlist1_01);
+      }
+    }
+  }
+</script>
---
+++ src/vue-components/vcFaqList1_01.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <article class="vcFaqList1-01">
+      <ul v-for="(i, index) in faqList">
+        <li>
+          <h2 class="vcFaqList1-01_title"
+          v-on:click="toggleContent(index)">
+            {{ i.title }}
+            <span v-if="!i.toggled">+</span>
+            <span v-else>-</span>
+          </h2>          
+          <section>
+            <p v-if="i.toggled">
+              {{ i.content }}
+            </p>
+          </section>
+        </li>
+      </ul>
+    </article>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      faqList: [
+        {
+          title: "vcFaqList1-01_title1",
+          content: "Lorem ipsum dolor sit amet1,...",
+          toggled: false
+        },
+        {
+          title: "vcFaqList1-01_title2",
+          content: "Lorem ipsum dolor sit amet2,...",
+          toggled: false
+        }
+      ]
+    }
+  },
+  mounted: function() {
+    this.toggleAllContent(false);
+  },
+  methods: {
+      toggleContent: function(index) {
+        let faqList = this.faqList;
+        faqList[index].toggled = !faqList[index].toggled;
+      },
+      toggleAllContent: function(visible) {
+        let faqList = this.faqList;
+        for (let i in faqList) {
+          faqList[i].toggled = visible;
+        }
+      }
+    }
+}
+</script>
